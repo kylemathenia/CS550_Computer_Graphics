69c251fc481edd8d0a18856a6bbda6e4218c6990
Kyle Mathenia <66032082+kylemathenia@users.noreply.github.com>
2022-09-13 20:05:16 -0700

    deleted unused file

--- ThreeBodySim/src/Shapes/shapes.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-GLuint getCubeList(float BOXSIZE);
-GLuint getSphereList(float radius, int slices, int stacks);
-GLuint getAxesList(const GLfloat axesWidth);
-GLuint getConeList(float radBot, float radTop, float height, int slices, int stacks,bool top, bool bot);
