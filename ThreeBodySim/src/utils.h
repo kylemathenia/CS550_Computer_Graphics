afdbeb65abbd51332fcb7292469b99a80d09cabd
Kyle Mathenia <66032082+kylemathenia@users.noreply.github.com>
2022-10-10 11:15:57 -0700

    updating lighting

--- ThreeBodySim/src/utils.h
+++ ThreeBodySim/src/utils.h
@@ -60,3 +60,18 @@ DoRasterString(float x, float y, float z, char* s);
 
 void
 DoStrokeString(float x, float y, float z, float ht, char* s);
+
+float*
+Array3(float a, float b, float c);
+
+float*
+MulArray3(float factor, float array0[3]);
+
+void
+SetMaterial(float r, float g, float b, float shininess);
+
+void
+SetPointLight(int ilight, float x, float y, float z, float r, float g, float b);
+
+void
+SetSpotLight(int ilight, float x, float y, float z, float xdir, float ydir, float zdir, float r, float g, float b);
